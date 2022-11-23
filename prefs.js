127b473ddb4e9e107f75eb3f1b8794f5aa6faa6a
Diego Hernandez <diego09hernandez@gmail.com>
2022-11-22 22:00:24 -0600

    Add re-size and username support

--- prefs.js
+++ prefs.js
@@ -5,6 +5,17 @@ const { Adw, Gio, Gtk } = imports.gi;
 const ExtensionUtils = imports.misc.extensionUtils;
 const Me = ExtensionUtils.getCurrentExtension();
 
+const { createRadio, createSlider, createSwitch } = Me.imports.utils.controls;
+
+const SETTINGS = Object.freeze({
+    MODE: 'avatar-mode',
+    POSITION: 'avatar-position',
+    SIZE: 'avatar-size',
+    REAL_NAME: 'avatar-realname',
+    USER_NAME: 'avatar-username',
+    HOST_NAME: 'avatar-hostname',
+    NO_BACKGROUND: 'avatar-nobackground',
+});
 
 function init() { }
 
@@ -12,34 +23,102 @@ function fillPreferencesWindow(window) {
     // Use the same GSettings schema as in `extension.js`
     const settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.quick-settings-avatar');
 
-    // Create a preferences page and group
+    // Create the preferences page
     const page = new Adw.PreferencesPage();
-    const group = new Adw.PreferencesGroup();
-    page.add(group);
 
-    const addToggleSetting = (settingId, settingTitle) => {
-        const row = new Adw.ActionRow({ title: settingTitle });
-        group.add(row);
+    // TODO: Future implementation
+    // Behavior Mode group
+    // const modeGroup = new Adw.PreferencesGroup();
+    // modeGroup.set_title('Behavior Mode');
+    // page.add(modeGroup);
 
-        // Create the switch and bind its value to the parametrized key
-        const toggle = new Gtk.Switch({
-            active: settings.get_boolean(settingId),
-            valign: Gtk.Align.CENTER,
-        });
+    const minimalOpt = createRadio({
+        title: 'Minimal',
+        subtitle: 'Opens the Users settings on click',
+        value: 0,
+        settings,
+        settingId: SETTINGS.MODE
+    });
+    const popupOpt = createRadio({
+        title: 'Pop up',
+        subtitle: 'Shows a pop up with user related actions on click',
+        value: 1,
+        settings,
+        settingId: SETTINGS.MODE,
+        group: minimalOpt.radio
+    });
 
-        settings.bind(
-            settingId,
-            toggle,
-            'active',
-            Gio.SettingsBindFlags.DEFAULT
-        );
+    // modeGroup.add(minimalOpt.row);
+    // modeGroup.add(popupOpt.row);
 
-        row.add_suffix(toggle);
-        row.activatable_widget = toggle;
-    };
+    // Position group
+    const positionGroup = new Adw.PreferencesGroup();
+    positionGroup.set_title('Position');
+    page.add(positionGroup);
 
-    addToggleSetting('is-avatar-on-left', 'Show user avatar on the left');
-    // addToggleSetting('is-large-avatar', 'Display a larger avatar');
+    const rightOpt = createRadio({
+        title: 'Right',
+        value: 0,
+        settings,
+        settingId: SETTINGS.POSITION
+    });
+    const leftOpt = createRadio({
+        title: 'Left',
+        value: 1,
+        settings,
+        settingId: SETTINGS.POSITION,
+        group: rightOpt.radio
+    });
+
+    positionGroup.add(rightOpt.row);
+    positionGroup.add(leftOpt.row);
+
+    // Appearance group
+    const appearanceGroup = new Adw.PreferencesGroup();
+    appearanceGroup.set_title('Appearance');
+    page.add(appearanceGroup);
+
+    const sizeControl = createSlider({
+        title: 'Size',
+        subtitle: '43 by default (matching the quick toggle icons)',
+        min: 15,
+        max: 75,
+        step: 2,
+        settings,
+        settingId: SETTINGS.SIZE
+    });
+
+    const realName = createSwitch({
+        title: 'Show real name',
+        subtitle: 'Depending on the name length, this will increase the panel width drastically',
+        settings,
+        settingId: SETTINGS.REAL_NAME
+    });
+
+    const userName = createSwitch({
+        title: 'Show user name',
+        settings,
+        settingId: SETTINGS.USER_NAME
+    });
+
+    const hostName = createSwitch({
+        title: 'Show host name',
+        settings,
+        settingId: SETTINGS.HOST_NAME
+    });
+
+    const noBackground = createSwitch({
+        title: 'Remove button background',
+        subtitle: 'Removes the default background that button toggles have',
+        settings,
+        settingId: SETTINGS.NO_BACKGROUND
+    });
+
+    appearanceGroup.add(sizeControl.row);
+    appearanceGroup.add(realName.row);
+    appearanceGroup.add(userName.row);
+    appearanceGroup.add(hostName.row);
+    appearanceGroup.add(noBackground.row);
 
     // Add our page to the window
     window.add(page);
